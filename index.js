4913d7a600f33c4019030e9e9ef29d9189c41749
Bernice Dwight <BDWIGHTNJ21@GMAIL.COM>
2023-04-20 17:39:11 -0400

    completed

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Code your solutions in this file
+
+const names=[`Guadalupe,`, `Ollie,`,`Aki,`];
+let messages = [];
+let eventName= "surprise"
+
+
+ function writeCards(names, eventName) {
+     
+   for (let i =0; i < names.length; i++) {
+    let message=`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`;      
+    //return [message];
+  
+   // return message;
+   messages.push(message);
+   
+   
+
+   
+}
+return messages; 
+    
+}
+     
+     
+
+
+   function countDown() {
+    for (var i = 10; i>= 0; i--) {
+     console.log(i);
+  
+     }
+     
+    }
